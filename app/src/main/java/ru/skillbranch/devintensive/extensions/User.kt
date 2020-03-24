d68deec4de25b22cb2f0cd864ebba6192fea1630
Shkiper01 <baga5828391@gmail.com>
2020-03-24 19:49:37 +0300

    first commit

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -25,26 +25,5 @@ fun User.toUserView(): UserView {
     )
 
 
-}
+} 
 
-fun String.truncate(i: Int = 16): String{
-    if(this.length < 16){
-        return this
-    }
-
-    var res = this.substring(0, i)
-
-    return res + "..."
-}
-
-fun String.stripHtml(): String{
-    var pos = this.split(">")
-    var result = pos[1]
-    while(result.contains("  ")){
-        val replace = result.replace("  ", " ")
-        result = replace
-    }
-
-
-    return result.replace("</p", "")
-}
