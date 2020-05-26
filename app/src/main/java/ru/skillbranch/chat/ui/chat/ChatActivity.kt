fdb4b2efff56958fa129269c7c926aaf1f3f57ce
Shkiper01 <baga5828391@gmail.com>
2020-05-26 20:35:23 +0300

    add title to toolbar

--- app/src/main/java/ru/skillbranch/chat/ui/chat/ChatActivity.kt
+++ app/src/main/java/ru/skillbranch/chat/ui/chat/ChatActivity.kt
@@ -1,16 +1,18 @@
 package ru.skillbranch.chat.ui.chat
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_chat.*
 import ru.skillbranch.chat.R
 import ru.skillbranch.chat.repositories.ChatRepository
 import ru.skillbranch.chat.ui.adapters.MessagesAdapter
 import ru.skillbranch.chat.utils.AppConstants
+import ru.skillbranch.chat.utils.Utils.toInitials
 import ru.skillbranch.chat.viewmodels.ChatViewModel
 
 
@@ -34,10 +36,23 @@ class ChatActivity : AppCompatActivity() {
 
     private fun initViews(){
         val chat = ChatRepository.find(intent.getStringExtra(AppConstants.CHAT_ID)!!)
-        chat ?: return
+
+        with(chat!!.members.first()) {
+            tv_title_chat.text = " $firstName"
+            if(avatar == null){
+                Glide.with(this@ChatActivity)
+                        .clear(iv_avatar_chat)
+                toInitials(firstName, lastName)?.let { iv_avatar_chat.setInitials(it) }
+            }
+            else{
+                Glide.with(this@ChatActivity)
+                        .load(avatar)
+                        .into(iv_avatar_chat)
+
+            }
+        }
 
         messagesAdapter = MessagesAdapter()
-
         messagesAdapter.updateData(chat.messages)
 
         with(rv_messages){
@@ -55,7 +70,7 @@ class ChatActivity : AppCompatActivity() {
 
     private fun initToolbar() {
         setSupportActionBar(toolbar_chat)
+        supportActionBar?.title = ""
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = intent.getStringExtra(AppConstants.CHAT_NAME)
     }
 }
